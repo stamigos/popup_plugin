bb46adebd2b95ed57397c7e04d9354d646ed4eea
stamigos <oldtigersvoice@gmail.com>
2016-08-01 17:23:09 +0300

    noConflict tries #13

--- core.js
+++ core.js
@@ -51,6 +51,35 @@ document.addEventListener('DOMContentLoaded', function() {
     includejQuery(function($){
         $jobufo = $.noConflict(true);
         console.log($jobufo)
+
+        $jobufo(document).ready(function(){
+
+			var require = {
+				domain: "http://212.24.111.125/",
+				html: "popup.php",
+				css: "popup.css",
+				js: "popup.js",
+				button: "img/main_button_nobg.gif"
+			}
+			var link_html = require.domain + require.html;
+			var link_css = require.domain + require.css;
+			var link_js = require.domain + require.js;
+			var link_button = require.domain + require.button;
+
+			// LOAD RESOURCES
+			$jobufo(document.body).append("<link href='" +link_css+ "' rel='stylesheet'>");
+			$jobufo(document.body).append("<div id='jobufo_back'></div>");
+			$jobufo(document.body).append("<div id='jobufo' style='display: none;'></div>");
+			$jobufo(document.body).append("<div id='jobufo_button' src='" +link_button+ "'></div>");
+
+			$jobufo("#jobufo").data("domain", require.domain);
+			$jobufo("#jobufo").load(link_html, function(){
+				// LOAD FIRST HTML, AFTER - JS
+				$jobufo(document.body).append("<script src='" +link_js+ "'>");
+			});
+			
+		});
+		
     });
 	// var html = "<script id='jobufo_script' src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>";
 	// appendHtml(document.body, html);
